76949ee15a1b96ca2ab7e9475ff6277577fee68d
brine <brian@qualityshepherd.com>
2018-05-30 17:46:23 -0500

    woops

--- pages/qsHomePage.js
+++ pages/qsHomePage.js
@@ -15,62 +15,8 @@ class QsHomePage extends BasePage {
         this.sidebar            = $('div#sidebar');
         // social media links....
         this.githubLink         = $('a#githubLink');
-        // pagination
-        this.prevPageLink       = $('a').withText('← Older Entries');
 
         this.url = 'http://qualityshepherd.com';
     }
-
-    /**
-     * check if a post title exists
-     * @param  {string} postTitle
-     * @return {bool}
-     */
-    postTitleExists(postTitle) {
-        return element(by.cssContainingText('a', postTitle)).isPresent();
-    }
-
-    /**
-     * Page back till we find the post title
-     * or run out of previous posts
-     * @param  {string} postTitle
-     * @return {bool}
-     */
-    findPostByPaging(postTitle) {
-        return this.postTitleExists(postTitle).then(found => {
-            if(found) {
-                // found it!
-                return true;
-            } else {
-                // prevPageLink not displayed on first page
-                this.prevPageLink.isPresent().then(yup => {
-                    if(yup) {
-                        this.prevPageLink.click();
-                        this.findPostByPaging(postTitle); // call recursively till found...
-                        // wait for page to load...
-                        this.loaded();
-                    } else {
-                        // post not found
-                        return false;
-                    }
-                });
-            }
-        });
-    }
 }
 export default new QsHomePage();
-
-
-// export default class Page {
-//     constructor () {
-//         this.loginInput    = $('#login');
-//         this.passwordInput = $('#password');
-//         this.signInButton  = $('#sign-in-button');
-//     }
-//     async login (t) {
-//         await t
-//             .typeText(this.loginInput, 'MyLogin')
-//             .typeText(this.passwordInput, 'Pa$word')
-//             .click(this.signInButton);
-//     }
-// }
